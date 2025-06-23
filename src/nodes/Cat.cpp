e40fa7473b3c5ee2b2fcbee633085ee82e84a8ce
abb2k <75845401+abb2k@users.noreply.github.com>
2025-06-23 07:43:54 +0300

    reverted

--- src/nodes/Cat.cpp
+++ src/nodes/Cat.cpp
@@ -27,8 +27,7 @@ bool Cat::init(CCNode* wanderArea) {
     this->setZOrder(100 / size + 1);
     this->setScale(size);
 
-    auto btn = CCMenuItem::create();
-    btn->setTarget(this, menu_selector(Cat::OnCatClicked));
+    auto btn = CCMenuItem::create(this, menu_selector(Cat::OnCatClicked));
     btn->setContentSize(this->getContentSize());
     btn->setPosition(this->getContentSize() / 2);
     btn->setID("general-kitty-btn");
